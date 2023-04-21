672ad66f937596765db9b4fdba5c73c08ebc4c2c
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2023-04-21 22:36:56 +0530

    Updates

---
+++ assets/preview-3ee55959.js
@@ -0,0 +1,2 @@
+const e={parameters:{layout:"centered",actions:{argTypesRegex:"^on[A-Z].*"},backgrounds:{disable:!0},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
+//# sourceMappingURL=preview-3ee55959.js.map
