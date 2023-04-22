a7ebe5d63c1502d8610f9344bed8fe18991cf9b1
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2023-04-23 01:19:38 +0530

    Updates

--- assets/WithTooltip-FBT32F6Q-f961434d.js
+++
@@ -1,2 +0,0 @@
-import{b as l,b as s,d as W}from"./index-09bbed5c.js";import"./iframe-c5fdd1fc.js";import"../sb-preview/runtime.mjs";import"./index-c6eaa83f.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-d475d2ea.js";import"./index-d37d4223.js";import"./index-477f9050.js";import"./index-356e4a49.js";export{l as WithToolTipState,s as WithTooltip,W as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-FBT32F6Q-f961434d.js.map
---
+++ assets/preview-dbee33e7.js
@@ -0,0 +1,2 @@
+const e={parameters:{layout:"centered",actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
+//# sourceMappingURL=preview-dbee33e7.js.map
